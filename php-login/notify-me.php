8745bf8ed19d26f653a1893d954010b98f2ba5d3
QuantStackz <github@quantstackz.com>
2017-04-04 14:25:47 -0700

    Update notify-me.php

--- php-login/notify-me.php
+++ php-login/notify-me.php
@@ -19,8 +19,7 @@ $STORE_FILE = $_SERVER["DOCUMENT_ROOT"]."/subscription-list.txt";
 /* ************************************ */
 // Don't forget to check the path below
 /* ************************************ */
- 
-require('MailChimp.php');
+ require('MailChimp.php');
 
 /* ***************************************************** */
 // For the part below, no interventions are required
@@ -37,24 +36,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["email"])) {
 	// Checking if the email writing is good
 	if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		
-		// The part for the storage in a .txt
-		if ($STORE_MODE == "file") {
-			
-			// SUCCESS SENDING
-			if(@file_put_contents($STORE_FILE, strtolower($email)."\r\n", FILE_APPEND)) {
-				echo json_encode(array(
-						"status" => "success"
-					));
-			// ERROR SENDING
-			} else {
-				echo json_encode(array(
-						"status" => "error",
-						"type" => "FileAccessError"
-					));
-			}
 		
 		// The part for the storage in Mailchimp
-		} elseif ($STORE_MODE == "mailchimp") {
+		if ($STORE_MODE == "mailchimp") {
 			
 			$MailChimp = new \Drewm\MailChimp($API_KEY);
 			
